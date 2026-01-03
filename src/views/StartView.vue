6fa5186020c14d98fa01ddb285146d2234a28541
ElysiaLiu <Elysialiu1230@gmail.com>
2026-01-03 15:02:53 +0100

    change the logo

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,43 +1,42 @@
 <template>
-  
+
   <div class="page">
-  <header>
-    <div v-bind:class="['hamburger', {'close': !hideNav}]" 
-         v-on:click="toggleNav">
-    </div>
-    <div class="logo">
-      <img src="/img/logo.png">
-      Polly polling tool 
-      <img src="../assets/logo.svg">
-    </div>
-  </header>
-  <ResponsiveNav v-bind:hideNav="hideNav">
-    <button v-on:click="switchLanguage">
-      {{ uiLabels.changeLanguage }}
-    </button>
-    <router-link to="/create/">
-      {{ uiLabels.createPoll }}
+    <header class="topbar">
+      <div v-bind:class="['hamburger', { 'close': !hideNav }]" v-on:click="toggleNav">
+      </div>
+      <div class="logo">
+        Easy Poll
+      </div>
+      <ResponsiveNav v-bind:hideNav="hideNav" class="nav">
+        <button v-on:click="switchLanguage">
+          {{ uiLabels.changeLanguage }}
+        </button>
+        <router-link to="/create/">
+          {{ uiLabels.createPoll }}
+        </router-link>
+        <a href="">
+          {{ uiLabels.about }}
+        </a>
+        <a href="">FAQ</a>
+      </ResponsiveNav>
+    </header>
+
+    <h1>{{ uiLabels["sales-pitch"] }}</h1>
+    <h2>{{ uiLabels.subHeading }}</h2>
+    <label>
+      Write poll id:
+      <input type="text" v-model="newPollId">
+    </label>
+    <router-link v-bind:to="'/lobby/' + newPollId">
+      {{ uiLabels.participatePoll }}
     </router-link>
-    <a href="">
-      {{ uiLabels.about }}
-    </a>
-    <a href="">FAQ</a>
-  </ResponsiveNav>
-  <h1>{{ uiLabels["sales-pitch"] }}</h1>
-  <h2>{{ uiLabels.subHeading }}</h2>
-  <label>
-    Write poll id: 
-    <input type="text" v-model="newPollId">
-  </label>
-  <router-link v-bind:to="'/lobby/' + newPollId">
-    {{ uiLabels.participatePoll }}
-  </router-link>
   </div>
 </template>
 
 <script>
 import ResponsiveNav from '@/components/ResponsiveNav.vue';
 import io from 'socket.io-client';
+
 const socket = io("localhost:3000");
 
 export default {
@@ -49,73 +48,65 @@ export default {
     return {
       uiLabels: {},
       newPollId: "",
-      lang: localStorage.getItem( "lang") || "en",
+      lang: localStorage.getItem("lang") || "en",
       hideNav: true
     }
   },
   created: function () {
-    socket.on( "uiLabels", labels => this.uiLabels = labels );
-    socket.emit( "getUILabels", this.lang );
+    socket.on("uiLabels", labels => this.uiLabels = labels);
+    socket.emit("getUILabels", this.lang);
   },
   methods: {
-    switchLanguage: function() {
+    switchLanguage: function () {
       if (this.lang === "en") {
         this.lang = "sv"
       }
       else {
         this.lang = "en"
       }
-      localStorage.setItem( "lang", this.lang );
-      socket.emit( "getUILabels", this.lang );
+      localStorage.setItem("lang", this.lang);
+      socket.emit("getUILabels", this.lang);
     },
     toggleNav: function () {
-      this.hideNav = ! this.hideNav;
+      this.hideNav = !this.hideNav;
     }
   }
 }
 </script>
 <style scoped>
-  .page {
-  min-height: 100vh;
-  background-color: #f7f7fb;
-  background-image: linear-gradient(
-    180deg,
-    rgba(123, 44, 191, 0) 0%,
-    rgba(0, 119, 255, 0.21) 100%
-  );
-}
-  header {
-    width: 100%;
-    display: grid;
-    grid-template-columns: 2em auto;
-  }
-  .logo {
-    text-transform: uppercase;
-    letter-spacing: 0.25em;
-    font-size: 2.5rem;
-    color: white;
-    padding-top:0.2em;
-  }
-  .logo img {
-    height:2.5rem;
-    vertical-align: bottom;
-    margin-right: 0.5rem; 
-  }
-  .hamburger {
-    color:white;
-    width:1em;
-    display: flex;
-    align-items: center;
-    justify-content: left;
-    padding:0.5rem;
-    top:0;
-    left:0;
-    height: 2rem;
-    cursor: pointer;
-    font-size: 1.5rem;
-  }
 
-@media screen and (max-width:50em) {
+.topbar {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 3.0rem auto 1fr; /* ☰ | logo | nav */
+  align-items: center;
+  column-gap: 24px;
+  padding: 0px 0px;
+}
+.nav {
+  justify-self: center;
+  display: flex;
+  align-items: center;
+  gap: 28px;
+  margin: 0;  /* 防止 nav 默认 margin 干扰 */
+}
+
+
+.hamburger {
+  color: white;
+  width: 1em;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  padding: 0.5rem;
+  top: 0;
+  left: 0;
+  height: 2rem;
+  cursor: pointer;
+  font-size: 1.5rem;
+}
+
+/* @media screen and (max-width:50em) {
   .logo {
     font-size: 5vw;
     display: flex;
@@ -131,5 +122,5 @@ export default {
   .hide {
     left:-12em;
   }
-}
+} */
 </style>
