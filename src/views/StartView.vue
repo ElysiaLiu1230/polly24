1e24e243d746d9c47797bd4353f01f4100931549
ElysiaLiu <Elysialiu1230@gmail.com>
2026-01-05 12:35:23 +0100

    change the homepage  css for both web and mobile

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,37 +1,56 @@
 <template>
-
   <div class="page">
     <header class="topbar">
-      <div v-bind:class="['hamburger', { 'close': !hideNav }]" v-on:click="toggleNav">
+      <div class="left">
+        <div :class="['navToggle', { close: !hideNav }]" @click="toggleNav"></div>
+        <div class="logo">Easy Poll</div>
       </div>
-      <div class="logo">
-        Easy Poll
-      </div>
-      <ResponsiveNav v-bind:hideNav="hideNav" class="nav">
-        <button v-on:click="switchLanguage">
-          {{ uiLabels.changeLanguage }}
-        </button>
-        <router-link to="/create/">
-          {{ uiLabels.createPoll }}
-        </router-link>
-        <a href="">
-          {{ uiLabels.about }}
-        </a>
+
+      <!-- web: nav in the middle -->
+      <ResponsiveNav :hideNav="hideNav" class="navDesktop">
+        <a href="#" @click.prevent="switchLanguage">{{ uiLabels.changeLanguage }}</a>
+        <router-link to="/create/">{{ uiLabels.createPoll }}</router-link>
+        <a href="">{{ uiLabels.about }}</a>
         <a href="">FAQ</a>
       </ResponsiveNav>
+
+      <div class="right">
+        <button class="iconBtn avatarDesktop" aria-label="Profile">
+          <span class="dot"></span>
+        </button>
+      </div>
     </header>
+
+    <!-- mobile：overlay + drawer -->
+    <div class="drawerWrap">
+      <div v-if="!hideNav" class="navOverlay" @click="toggleNav"></div>
+
+      <ResponsiveNav :hideNav="hideNav" class="navMobile">
+        <a href="#" @click.prevent="switchLanguage">{{ uiLabels.changeLanguage }}</a>
+        <router-link to="/create/">{{ uiLabels.createPoll }}</router-link>
+        <a href="">{{ uiLabels.about }}</a>
+        <a href="">FAQ</a>
+
+        <button class="iconBtn avatarMobile" aria-label="Profile">
+          <span class="dot"></span>
+        </button>
+      </ResponsiveNav>
+    </div>
 
     <h1>{{ uiLabels["sales-pitch"] }}</h1>
     <h2>{{ uiLabels.subHeading }}</h2>
+
     <label>
       Write poll id:
-      <input type="text" v-model="newPollId">
+      <input type="text" v-model="newPollId" />
     </label>
-    <router-link v-bind:to="'/lobby/' + newPollId">
+
+    <router-link :to="'/lobby/' + newPollId">
       {{ uiLabels.participatePoll }}
     </router-link>
   </div>
 </template>
+
 
 <script>
 import ResponsiveNav from '@/components/ResponsiveNav.vue';
@@ -74,26 +93,58 @@ export default {
 }
 </script>
 <style scoped>
-
 .topbar {
-  width: 100%;
   display: grid;
-  grid-template-columns: 3.0rem auto 1fr; 
+  grid-template-columns: 1fr auto 1fr;
   align-items: center;
-  column-gap: 24px;
-  padding: 0px 0px;
+  padding: 0 1rem;
 }
-.nav {
+
+
+.left {
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+}
+
+.navDesktop {
   justify-self: center;
   display: flex;
   align-items: center;
-  gap: 28px;
+  gap: 1.75rem;
   margin: 0;
 }
 
 
-.hamburger {
-  color: white;
+
+.right {
+  justify-self: end;
+  /* adjust to the right */
+  display: flex;
+  align-items: center;
+}
+
+.iconBtn {
+  width: 2rem;
+  height: 2rem;
+  border-radius: 999px;
+  border: 1px solid #999;
+  background: transparent;
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+}
+
+.dot {
+  width: 0.6rem;
+  height: 0.6rem;
+  border-radius: 999px;
+  background: #999;
+}
+
+
+.navToggle {
+  color: rgb(239, 165, 165);
   width: 1em;
   display: flex;
   align-items: center;
@@ -104,23 +155,109 @@ export default {
   height: 2rem;
   cursor: pointer;
   font-size: 1.5rem;
+  z-index: 1200;
 }
 
-/* @media screen and (max-width:50em) {
+
+
+/* desktop for web */
+.navMobile {
+  display: none;
+}
+.avatarMobile {
+  display: none;
+}
+.navOverlay {
+  display: none;
+}
+/* desktop for mobile */
+@media screen and (max-width: 50em) {
+
+  /* hide the drawer */
+  .navDesktop {
+    display: none;
+  }
+
+  .avatarDesktop {
+    display: none;
+  }
+
+  /* show the drawer */
+  .navMobile {
+    display: block;
+  }
+
+  .avatarMobile {
+    display: inline-grid;
+  }
+
+  /* change the logo size*/
   .logo {
     font-size: 5vw;
     display: flex;
     align-items: center;
     justify-content: center;
   }
-  .hamburger::before {
+
+  .navToggle::before {
     content: "☰";
   }
+
   .close::before {
     content: "✕";
   }
-  .hide {
-    left:-12em;
+
+  /* change the direction of the menu */
+  .navMobile {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
   }
-} */
+
+  /* make the menu item block */
+  .navMobile>* {
+    display: block;
+    width: 100%;
+    padding: 0.9rem 1rem;
+    line-height: 1.2;
+    text-align: left;
+  }
+
+  /* move the link inside the menu item */
+  .navMobile a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+    /* overlay for drawer */
+  .navOverlay {
+    display: block;
+    position: fixed;
+    inset: 0;
+    background: rgba(255, 255, 255, 0.9);
+    z-index: 900;
+  }
+
+  /* ensure the navMobile is above the navOverlay */
+  .navMobile {
+    z-index: 1000;
+  }
+
+  .navMobile .avatarMobile {
+    width: 100%;
+    height: auto;
+    border-radius: 0;
+    border: none;
+    background: none;
+    display: flex;
+    align-items: center;
+    gap: 0.6rem;
+    padding: 0.9rem 1rem;
+    justify-content: flex-start;
+  }
+
+  .navMobile .avatarMobile::after {
+    content: "Profile";
+  }
+}
 </style>
